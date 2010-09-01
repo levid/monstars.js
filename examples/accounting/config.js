95633eae235929faa568d95f1fd6c6c1d10debe2
Sean <sean@mcarthurgfx.com>
2010-08-31 22:41:43 -0700

    a small tasks example

--- examples/accounting/config.js
+++
@@ -1,7 +0,0 @@
-init.core();
-init.mvc('Model.Ajax','Controller.Drag');
-init.app(function() {
-	init.models('Expense');
-	init.controllers('Expenses');
-	init.views('expenses/index'); //should be able to not have to do this. things in here will loaded at start
-});
