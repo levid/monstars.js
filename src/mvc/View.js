3f4830f3c56b358b39adc1a5435444a973f30b92
Sean McArthur <sean.monstar@gmail.com>
2010-12-03 15:29:27 -0800

    reducing the html escape code in view rendering

--- src/mvc/View.js
+++ src/mvc/View.js
@@ -28,7 +28,7 @@ var View = new Class({
 					.split("<%").join("\t")
 					.replace(/((^|%>)[^\t]*)'/g, "$1\r")
 					.replace(/\t=(.*?)%>/g, "',$1,'")
-					.replace(/\t:(.*?)%>/g, "',document.createElement('div').appendChild(document.createTextNode($1)).parentNode.innerHTML,'")
+					.replace(/\t:(.*?)%>/g, "',new Element('div')set('text',$1).get('html'),'")
 					.split("\t").join("');")
 					.split("%>").join("p.push('")
 					.split("\r").join("\\'")
